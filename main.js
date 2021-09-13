5738770c06f81a8ef520aec4318358f9cafaf192
prisha-76 <74600510+prisha-76@users.noreply.github.com>
2021-09-13 18:32:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+/*created by prashant shukla */
+
+var paddle2 =10,paddle1=10;
+
+var paddle1X = 10,paddle1Height = 110;
+var paddle2Y = 685,paddle2Height = 70;
+
+var score1 = 0, score2 =0;
+var paddle1Y;
+
+var  playerscore =0;
+
+var pcscore =0;
+
+var ball = {
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
+}
+
